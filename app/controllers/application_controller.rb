43663b7ec65b7f040ae523e4540e7e44786fe89a
designblooz <paulkim@designblooz.com>
2012-11-24 01:41:50 -0500

    Update app/controllers/application_controller.rb
    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
   before_filter :sidebar
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "foo" && password == "bar"
+    end
+  end
+
   def sidebar
   	@boardsList = Board.all
   end
+
 end
