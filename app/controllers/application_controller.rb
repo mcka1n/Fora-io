8eca778cdfe3377922f6b9c240a34a91de7a4975
root <root@li557-222.members.linode.com>
2012-11-24 03:43:55 -0500

    authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate 
   before_filter :sidebar
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+        username == "fora" && password == "craigslistkiller"
+    end
+    warden.custom_failure! if performed?
+  end
 
   def sidebar
   	@boardsList = Board.all
