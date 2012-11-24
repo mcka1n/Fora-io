0068ad392716bc3b31960019b3904b341621af52
designblooz <paulkim@designblooz.com>
2012-11-24 01:58:24 -0500

    Update app/controllers/application_controller.rb
    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate
   before_filter :sidebar
-
-  def authenticate
-    authenticate_or_request_with_http_basic do |user_name, password|
-      # Change these to username and password required
-      user_name == "fora" && password == "gobeta"
-    end
-  end
 
   def sidebar
   	@boardsList = Board.all
